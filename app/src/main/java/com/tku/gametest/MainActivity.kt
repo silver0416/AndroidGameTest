9cd5ed481d864ef06ece469f565e44f43bbcb3df
Cgost <colorghost1130@gmail.com>
2023-10-29 13:03:08 +0800

    新增工具列

--- app/src/main/java/com/tku/gametest/MainActivity.kt
+++ app/src/main/java/com/tku/gametest/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.webkit.WebViewAssetLoader
 import androidx.webkit.WebViewClientCompat
@@ -20,6 +21,34 @@ class MainActivity : ComponentActivity() {
         val webView: WebView = findViewById(R.id.webView)
         webView.settings.javaScriptEnabled = true
 
+        // 初始化按鈕
+        val buttonBack: Button = findViewById(R.id.buttonBack)
+        val buttonForward: Button = findViewById(R.id.buttonForward)
+        val buttonRefresh: Button = findViewById(R.id.buttonRefresh)
+        val buttonHome: Button = findViewById(R.id.buttonHome)
+
+        // 為按鈕添加點擊事件監聽器
+        buttonBack.setOnClickListener {
+            if (webView.canGoBack()) {
+                webView.goBack()
+            }
+        }
+
+        buttonForward.setOnClickListener {
+            if (webView.canGoForward()) {
+                webView.goForward()
+            }
+        }
+
+        buttonRefresh.setOnClickListener {
+            webView.reload()
+        }
+
+        buttonHome.setOnClickListener {
+            webView.loadUrl("https://appassets.androidplatform.net/assets/index.html")
+        }
+
+        // WebView 資源載入
         val assetLoader = WebViewAssetLoader.Builder()
             .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
             .addPathHandler("/res/", WebViewAssetLoader.ResourcesPathHandler(this))
@@ -29,7 +58,6 @@ class MainActivity : ComponentActivity() {
         webView.loadUrl("https://appassets.androidplatform.net/assets/index.html")
     }
 }
-
 
 private class LocalContentWebViewClient(private val assetLoader: WebViewAssetLoader) : WebViewClientCompat() {
     override fun shouldInterceptRequest(
