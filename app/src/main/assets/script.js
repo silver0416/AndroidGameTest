d031b787770798f45c4ced92ec2ac94455e0ccf1
DESKTOP-335ND2V\陳柏諭 <564732.eee27>
2023-11-14 16:35:30 +0800

    修正翻牌小遊戲的顯示問題

--- app/src/main/assets/script.js
+++ app/src/main/assets/script.js
@@ -1,5 +1,16 @@
 var gameListing = document.getElementById("game-list");
 
+window.onload = function() {
+    // 使用 URLSearchParams 來解析 URL
+    var urlParams = new URLSearchParams(window.location.search);
+
+    // 使用 get 方法來獲得參數的值
+    var myParam = urlParams.get('param');
+
+    // 使用該參數，例如在一個元素中顯示它
+    document.getElementById("display").innerText = myParam;
+}
+
 games().then(function(gamePaths) {
     gamePaths.forEach(function(gamePath) {
         const imgElement = document.createElement('img');
